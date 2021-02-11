d51921703ec674ce7a8e59b788d6e320477561db
ZenRega38 <regarizz@gmail.com>
2021-02-11 10:44:41 +0800

    fix a bit

--- js/main.js
+++ js/main.js
@@ -17,8 +17,8 @@ function getMovies(searchText){
 				<div class="col-md-3">
 					<div class="well text-center">
 						<img src="${movie.Poster}">
-						<h5>${movie.Title}</h5>
-						<a onClick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Detail</a>
+						<h5 style="padding:10px; weight:3;">${movie.Title}</h5>
+						<a style="item-weight:bottom;" onClick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Detail</a>
 					</div>
 				</div>
 			`;
@@ -56,7 +56,7 @@ function getMovie(){
 						<li class"list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
 						<li class"list-group-item"><strong>Released:</strong> ${movie.Released}</li>
 						<li class"list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
-						<li class"list-group-item"><strong>IMDB:</strong> ${movie.imdbRating}</li>
+						<li class"list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
 						<li class"list-group-item"><strong>Director:</strong> ${movie.Director}</li>
 						<li class"list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
 						<li class"list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
